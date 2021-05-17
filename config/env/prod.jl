5b51a819f420a37b14e9ee057e24c55178078b2f
gzagatti <gzagatti@users.noreply.github.com>
2021-05-18 00:58:52 +0800

    modify prod configurations.

--- config/env/prod.jl
+++ config/env/prod.jl
@@ -3,7 +3,7 @@ using Genie.Configuration, Logging
 const config = Settings(
   server_port                     = 8000,
   server_host                     = "0.0.0.0",
-  log_level                       = Logging.Error,
+  log_level                       = Logging.Info,
   log_to_file                     = true,
   server_handle_static_files      = false
 )
